738243df4a7455a8515857005ef3068b7fb4c81b
ella <pihamella@gmail.com>
2022-07-19 11:05:33 +0100

    V1

---
+++ database/migrations/2022_07_13_104854_create__consultation.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_consultation', function (Blueprint $table) {
+            $table->id();
+            $table->String('Type_Consultation');
+            $table->Date('Date_consultation');
+            $table->String('Prix_consultation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_consultation');
+    }
+};
